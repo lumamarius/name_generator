86d0d67dbdad932b49ec4b110693d2cf1748180d
MARIUS WERTHER <marius.werther@proton.me>
2024-10-27 07:58:48 +0100

    Added new line for input.

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from time import sleep
 
 print("Welcome to the Generator!")
-print("What is your Main Character?")
-main_character = input("Main: ")
+main_character = input("What is your Main Character?\n")
 print(f"Cool! Your Main Character is {main_character}!")
-user_name = input("Whats your Name? ")
+user_name = input("Whats your Name?\n")
 print(f"Nice to meet you, {user_name}!")
 sleep(3)
 print(f"Your name could be {main_character} {user_name} or {user_name} {main_character}!")
 sleep(3)
 print("Yes, the Generator is really useless. Thanks for trying it out tho! :)")
-
-print("Date of Creation: 26.10.2024 - Marius") #Just for me, to check my progress sometimes.
+sleep(3)
+print("Date of Creation: 26.10.2024 - Last Update: 27.10.2024 - Marius") #Just for me, to check my progress sometimes.
